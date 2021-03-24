75f74ceb5cdd7d678f6eb7b38d9c204d9deb5232
kgn1003 <39154288+kgn1003@users.noreply.github.com>
2021-03-24 15:35:04 +0900

    Add files via upload

---
+++ BaekJoon/[1149]RGB거리.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int N, RGB[1001][4], answer[1001][4];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= 3; j++) {
+			cin >> RGB[i][j];
+		}
+	}
+	answer[1][1] = RGB[1][1]; answer[1][2] = RGB[1][2]; answer[1][3] = RGB[1][3];
+
+	for (int i = 2; i <= N; i++) {
+			answer[i][1] = (answer[i - 1][2] < answer[i - 1][3] ? answer[i - 1][2] : answer[i - 1][3]) +RGB[i][1];
+			answer[i][2] = (answer[i - 1][1] < answer[i - 1][3] ? answer[i - 1][1] : answer[i - 1][3]) +RGB[i][2];
+			answer[i][3] = (answer[i - 1][1] < answer[i - 1][2] ? answer[i - 1][1] : answer[i - 1][2]) +RGB[i][3];
+	}
+
+	int ans = answer[N][1];
+	for (int i = 2; i <= 3; i++) {
+		ans = ans < answer[N][i] ? ans : answer[N][i];
+	}
+	cout << ans;
+	return 0;
+}
