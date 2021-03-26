0c25bbe064a3ecb4d16ba1b68ac62bb85b0ceb4a
kgn1003 <39154288+kgn1003@users.noreply.github.com>
2021-03-26 12:29:30 +0900

    Add files via upload
    map

---
+++ BaekJoon/[10816]숫자카드2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+long long N, M,answer[50001];
+map<int, int> m;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		m[a]++;
+	}
+
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		int a;
+		cin >> a;
+		cout << m[a] << " ";
+	}
+
+}
