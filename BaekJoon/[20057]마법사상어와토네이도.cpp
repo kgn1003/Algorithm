5ff5c692efb70a14ed7c8ff4d3e7f9029711f435
kgn1003 <39154288+kgn1003@users.noreply.github.com>
2021-04-22 15:27:17 +0900

    Add files via upload

---
+++ BaekJoon/[20057]마법사상어와토네이도.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int N, dy[] = {0,1,0,-1}, dx[] = {-1,0,1,0}, answer;
+vector<pair<pair<int, int>,int>> pos[4] = {
+	{ {{-1,-1},10},{{-1,0},7},{{-1,1},1},{{-2,0},2},{{0,-2},5},{{1,-1},10},{{1,0},7},{{1,1},1},{{2,0},2}}, // аб
+{{{-1,-1},1},{{0,-1},7},{{0,-2},2},{{1,-1},10},{{2,0},5},{{1,1},10},{{0,1},7},{{0,2},2},{{-1,1},1}}, // го
+{{{-1,-1},1},{{-1,0},7},{{-1,1},10},{{-2,0},2},{{0,2},5},{{1,-1},1},{{1,0},7},{{1,1},10},{{2,0},2}}, // ©Л
+{{{-1,-1},10},{{0,-1},7},{{1,-1},1},{{0,-2},2},{{-2,0},5},{{1,1},1},{{0,1},7},{{0,2},2},{{-1,1},10}} // ╩С
+};
+int A[501][501], map[501][501];
+
+int solution(int sy, int sx, int dist) {
+	while (true) {
+		int idx = 2;
+		if (dist % 2) {
+			idx = 0;
+		}
+		for (int cnt = 0; cnt < 2; cnt++) {
+
+			for (int i = 0; i < dist; i++) {
+
+				int y = sy, x = sx;
+				sy = y + dy[idx]; sx = x + dx[idx];
+				int ans = A[sy][sx];
+				int temp = 0; 
+				for (int j = 0; j < pos[idx].size(); j++) {
+
+					int ny = sy + pos[idx][j].first.first, nx = sx + pos[idx][j].first.second;
+					if (ny >= 1 && ny <= N && nx >= 1 && nx <= N) {
+
+						A[ny][nx] += (A[sy][sx]) * (pos[idx][j].second)/100;
+						temp += (A[sy][sx] * pos[idx][j].second)/100;
+					
+					}
+					else {
+						temp += (A[sy][sx] * pos[idx][j].second) / 100;
+
+						answer += (A[sy][sx] * pos[idx][j].second)/100;
+					}
+
+				}
+
+				A[sy][sx] = 0;
+
+				int ny = sy + dy[idx], nx = sx + dx[idx];
+				if (ny >= 1 && ny <= N && nx >= 1 && nx <= N) {
+					A[ny][nx] += ans - temp;
+				}
+				else {
+					answer += ans - temp;
+				}
+				if (sy == 1 && sx == 1)
+					return answer;
+				map[sy][sx] = dist;
+				
+			}
+			idx++;
+		}
+		//cout <<"-------------------------------\n";
+		//for (int a = 1; a <= N; a++) {
+		//	for (int b = 1; b <= N; b++) {
+		//		cout << A[a][b] << " ";
+		//	}
+		//	cout << endl;
+		//}
+
+		//cout << "---------------------" << endl;
+		//for (int a = 1; a <= N; a++) {
+		//	for (int b = 1; b <= N; b++) {
+		//		cout << map[a][b] << " ";
+		//	}
+		//	cout << endl;
+		//}
+		dist++;
+	}
+	return answer;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	int sy = (N / 2) + 1, sx = (N/2)+1;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> A[i][j];
+		}
+	}
+
+	cout << solution(sy, sx, 1);
+	return 0;
+}
