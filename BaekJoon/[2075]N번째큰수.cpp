42fd7136303030fb51ddec92af7afa8b22e2cafa
kgn1003 <39154288+kgn1003@users.noreply.github.com>
2021-05-10 15:33:43 +0900

    Update [2075]N번째큰수.cpp

--- BaekJoon/[2075]N번째큰수.cpp
+++ BaekJoon/[2075]N번째큰수.cpp
@@ -30,3 +30,30 @@ int main() {
 	cout << pq.top();
 	return 0;
 }
+
+/////*vector sort and erase*//////////
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> v;
+int N, num;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> num;
+			v.push_back(num);
+		}
+		sort(v.begin(), v.end(),greater<int>());
+		if (v.size() > N) {
+			v.erase(v.begin() + N, v.end());
+		}
+	}
+	
+	cout << v[N - 1];
+	return 0;
+}
